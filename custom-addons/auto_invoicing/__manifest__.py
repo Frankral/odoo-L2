abd0f32fd8041b00b5aaa6d8cc4324415ddee113
Alberto Michaël <albermichh@gmail.com>
2024-11-22 15:33:43 +0300

    feat: ajout de dépendance

--- custom-addons/auto_invoicing/__manifest__.py
+++ custom-addons/auto_invoicing/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "17.0.1.0",
 
     'depends': [
-        'base', 'sale', 'stock'
+        'base', 'sale', 'sale_management', 'stock', 'purchase', 'sale_stock'
     ],
 
     'data': [
